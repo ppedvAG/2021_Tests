8b5c0ad6c26197e1f302614032c1f879901d7cd2
André <ruland@outlook.de>
2021-03-19 15:56:46 +0100

    UI Console

---
+++ ppedv.Zugfahrt/ppedv.Zugfahrt.UI.DevConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using ppedv.Zugfahrt.Data.Ef;
+using ppedv.Zugfahrt.Logic;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace ppedv.Zugfahrt.UI.DevConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Hello World!");
+
+            var core = new Core(new EfRepository());
+
+            foreach (var zf in core.Repository.Query<Model.Zugfahrt>().Where(x => x.Datum.Month < 15).ToList())
+            {
+                Console.WriteLine($"{zf.Start} - { zf.Ende} {zf.Datum:dd.MMM yyyy}");
+                foreach (var t in zf.Tickets)
+                {
+                    Console.WriteLine($"\t{t.Kunde.Name} {t.Preis:c}");
+                }
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
