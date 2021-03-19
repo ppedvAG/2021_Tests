34ab9e36720483ade36da23999556ca163aeacd0
André <ruland@outlook.de>
2021-03-19 15:41:49 +0100

    Logic

---
+++ ppedv.Zugfahrt/ppedv.Zugfahrt.Data.Ef/EfRepository.cs
@@ -0,0 +1,52 @@
+﻿using ppedv.Zugfahrt.Model;
+using ppedv.Zugfahrt.Model.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ppedv.Zugfahrt.Data.Ef
+{
+    public class EfRepository : IRepository
+    {
+        EfContext con = new EfContext();
+
+        public void Add<T>(T entity) where T : Entity
+        {
+            //if (entity is Kunde)
+            //    con.Kunden.Add(entity as Kunde);
+            con.Set<T>().Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : Entity
+        {
+            con.Set<T>().Remove(entity);
+        }
+
+        public IEnumerable<T> GetAll<T>() where T : Entity
+        {
+            return con.Set<T>().ToList();
+        }
+
+        public T GetById<T>(int id) where T : Entity
+        {
+            return con.Set<T>().Find(id);
+        }
+
+        public IQueryable<T> Query<T>() where T : Entity
+        {
+            return con.Set<T>();
+        }
+
+        public void SaveAll()
+        {
+            con.SaveChanges();
+        }
+
+        public void Update<T>(T entity) where T : Entity
+        {
+            var loaded = GetById<T>(entity.Id);
+            if (loaded != null)
+                con.Entry(loaded).CurrentValues.SetValues(entity);
+        }
+    }
+}
---
+++ ppedv.Zugfahrt/ppedv.Zugfahrt.Logic/Core.cs
@@ -0,0 +1,27 @@
+﻿using ppedv.Zugfahrt.Model.Contracts;
+using System;
+using System.Linq;
+
+namespace ppedv.Zugfahrt.Logic
+{
+    public class Core
+    {
+        public IRepository Repository { get; }
+
+        public Core(IRepository repository)
+        {
+            Repository = repository;
+        }
+
+        public decimal CalcFahrtEinnahmen(Model.Zugfahrt fahrt)
+        {
+            //komplexe Business Logic
+            return fahrt.Tickets.Sum(x => x.Preis);
+        }
+
+        public Model.Zugfahrt GetMostValueZugfahrt()
+        {
+            return Repository.Query<Model.Zugfahrt>().OrderBy(x => x.Tickets.Sum(y => y.Preis)).FirstOrDefault();
+        }
+    }
+}
