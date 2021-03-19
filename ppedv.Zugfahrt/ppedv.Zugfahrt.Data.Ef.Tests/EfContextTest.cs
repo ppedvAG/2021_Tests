f266b29b55b6f993892baeea50795bbe8362025b
André <ruland@outlook.de>
2021-03-19 12:00:11 +0100

    EF konfiguriert und DB Create Test

---
+++ ppedv.Zugfahrt/ppedv.Zugfahrt.Data.Ef.Tests/EfContextTest.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace ppedv.Zugfahrt.Data.Ef.Tests
+{
+    public class EfContextTest
+    {
+        [Fact]
+        public void Can_create_DB()
+        {
+            var con = new EfContext();
+
+            if (con.Database.Exists())
+                con.Database.Delete();
+
+            con.Database.Create();
+
+            Assert.True(con.Database.Exists());
+        }
+
+    }
+}
---
+++ ppedv.Zugfahrt/ppedv.Zugfahrt.Data.Ef/EfContext.cs
@@ -0,0 +1,25 @@
+﻿using ppedv.Zugfahrt.Model;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ppedv.Zugfahrt.Data.Ef
+{
+    public class EfContext : DbContext
+    {
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Kunde> Kunden { get; set; }
+        public DbSet<Model.Zugfahrt> Fahrten { get; set; }
+
+        public EfContext(string conString) : base(conString)
+        { }
+
+        public EfContext() : this("Server=(localdb)\\mssqllocaldb;Database=Zugfahrt_dev;Trusted_Connection=true")
+        { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
